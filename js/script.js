1be6ce778a6de934203e3463d0fa67c339953963
Leonel <leonelvalentinn@gmail.com>
2023-12-19 16:06:42 -0600

    Página Full

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ const swiper = new Swiper('.sample-slider', {
     delay: 2000,  
     disableOnInteraction: false,
   },       
+  effect: 'fade',         // apply fade effect
+    fadeEffect: {           
+      crossFade: true     // resolve the overlapping of the slides
+  },
   navigation: {                       //navigation(arrow)
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -13,7 +17,8 @@ const swiper = new Swiper('.sample-slider', {
   },
 })
 const swiper1 = new Swiper('.sample-slider-1', {
-  loop: true,                         //loop
+  loop: true, 
+  spaceBetween: 30,                        //loop
   autoplay: {                         //autoplay
     delay: 2000,  
     disableOnInteraction: false,
@@ -29,6 +34,13 @@ const swiper1 = new Swiper('.sample-slider-1', {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  // Responsive breakpoints
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    }
+  }
 })
 
 
@@ -39,6 +51,13 @@ const swiper1 = new Swiper('.sample-slider-1', {
       delay: 2000,  
       disableOnInteraction: false,
     },   
+    // Responsive breakpoints
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      }
+    }
   });
 
 const bg = document.getElementById('header')
@@ -51,44 +70,102 @@ window.onscroll = function() {
   if (y==0) {
     bg.classList.remove('header-blanco')
   }
-  if (y>=3200) {
-    const counts = document.querySelectorAll('.count_amount')
-    const velocidad = 700;
-
-    const animationCount = () =>{
-        for(const count of counts){
-            const countUpdate = () => {
-                let cantidadMaxima = +count.dataset.cantidadTotal,
-                valor_actual = +count.innerText,
-                incremento = cantidadMaxima / velocidad
-                if(valor_actual < cantidadMaxima){
-                    count.innerText = Math.ceil(valor_actual + incremento)
-                    setTimeout(countUpdate, 5)
-                }else{
-                    count.innerText = cantidadMaxima
-                }
-            }
-            countUpdate()
-        }
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    /* La pantalla tiene al menos 768 píxeles de ancho */
+    if (y>=1580) {
+      const counts = document.querySelectorAll('.count_amount')
+      const velocidad = 700;
+  
+      const animationCount = () =>{
+          for(const count of counts){
+              const countUpdate = () => {
+                  let cantidadMaxima = +count.dataset.cantidadTotal,
+                  valor_actual = +count.innerText,
+                  incremento = cantidadMaxima / velocidad
+                  if(valor_actual < cantidadMaxima){
+                      count.innerText = Math.ceil(valor_actual + incremento)
+                      setTimeout(countUpdate, 5)
+                  }else{
+                      count.innerText = cantidadMaxima
+                  }
+              }
+              countUpdate()
+          }
+      }
+      const mostrarCount = elementos => {
+          elementos.forEach(elemento => {
+              if(elemento.isIntersecting){
+                  setTimeout(animationCount, 300)
+              }
+          });
+      }
+      const observer = new IntersectionObserver(mostrarCount, {
+          threshold: 0.75
+      })
+      const elementosHTML = document.querySelectorAll('.contador')
+      elementosHTML.forEach(elementoHTML => {
+          observer.observe(elementoHTML)
+      })
     }
-    const mostrarCount = elementos => {
-        elementos.forEach(elemento => {
-            if(elemento.isIntersecting){
-                setTimeout(animationCount, 300)
-            }
-        });
+  } else {
+    /* La pantalla tiene menos de 768 píxeles de ancho */
+    if (y>=3200) {
+      const counts = document.querySelectorAll('.count_amount')
+      const velocidad = 700;
+  
+      const animationCount = () =>{
+          for(const count of counts){
+              const countUpdate = () => {
+                  let cantidadMaxima = +count.dataset.cantidadTotal,
+                  valor_actual = +count.innerText,
+                  incremento = cantidadMaxima / velocidad
+                  if(valor_actual < cantidadMaxima){
+                      count.innerText = Math.ceil(valor_actual + incremento)
+                      setTimeout(countUpdate, 5)
+                  }else{
+                      count.innerText = cantidadMaxima
+                  }
+              }
+              countUpdate()
+          }
+      }
+      const mostrarCount = elementos => {
+          elementos.forEach(elemento => {
+              if(elemento.isIntersecting){
+                  setTimeout(animationCount, 300)
+              }
+          });
+      }
+      const observer = new IntersectionObserver(mostrarCount, {
+          threshold: 0.75
+      })
+      const elementosHTML = document.querySelectorAll('.contador')
+      elementosHTML.forEach(elementoHTML => {
+          observer.observe(elementoHTML)
+      })
     }
-    const observer = new IntersectionObserver(mostrarCount, {
-        threshold: 0.75
-    })
-    const elementosHMTL = document.querySelectorAll('.contador')
-    elementosHMTL.forEach(elementoHMTL => {
-        observer.observe(elementoHMTL)
-    })
   }
 }
 
 const logo = document.getElementById('logo')
 logo.addEventListener('click', function () {
   location.assign('/')
+})
+
+const abrir = document.getElementById('open')
+const menu = document.getElementById('menu')
+abrir.addEventListener('click', function () {
+  menu.classList.add('show-menu')
+})
+const cerrar = document.getElementById('close')
+cerrar.addEventListener('click', function () {
+  menu.classList.remove('show-menu')
+})
+
+const menucon = document.getElementById('menu-contacto')
+const whats = document.getElementById('whats')
+const tel = document.getElementById('tel')
+menucon.addEventListener('click', function () {
+  whats.classList.toggle('show-whats')
+  tel.classList.toggle('show-tel')
 })
